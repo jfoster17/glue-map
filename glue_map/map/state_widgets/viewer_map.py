9f16a8b3375b4c03caf4175603d8d7dbc5afcbce
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2022-10-31 11:58:04 -0400

    Copy scatter viewer for very simple viewer options (does not display sidebar on top)

--- glue_map/map/state_widgets/viewer_map.py
+++ glue_map/map/state_widgets/viewer_map.py
@@ -1,26 +1,18 @@
-import ipyvuetify as v
-import traitlets
-from glue_jupyter.state_traitlets_helpers import GlueState
-from glue_jupyter.vuetify_helpers import link_glue_choices
+from ipywidgets import VBox, Checkbox
+
+from glue_jupyter.widgets.linked_dropdown import LinkedDropdown
 
 __all__ = ['MapViewerStateWidget']
 
 
-class MapViewerStateWidget(v.VuetifyTemplate):
-    template_file = (__file__,'viewer_map.vue')
-
-    lon_att_items = traitlets.List().tag(sync=True)
-    lon_att_selected = traitlets.Int(allow_none=True).tag(sync=True)
-    lat_att_items = traitlets.List().tag(sync=True)
-    lat_att_selected = traitlets.Int(allow_none=True).tag(sync=True)
+class MapViewerStateWidget(VBox):
 
     def __init__(self, viewer_state):
         super().__init__()
         
-        self.glue_state = viewer_state
+        self.state = viewer_state
         
-        link_glue_choices(self, viewer_state, 'lon_att')
-        link_glue_choices(self, viewer_state, 'lat_att')
+        self.widget_lon_axis = LinkedDropdown(self.state, 'lon_att', label='lon_att')
+        self.widget_lat_axis = LinkedDropdown(self.state, 'lat_att', label='lat_att')
         
-    def cleanup(self):
-        pass
+        super().__init__([self.widget_lon_axis, self.widget_lat_axis])
