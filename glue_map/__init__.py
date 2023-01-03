c7181513e5a822a2b7e6922d94d87f36c83451b6
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2023-01-03 14:33:13 -0500

    Add some extra colormaps for maps

--- glue_map/__init__.py
+++ glue_map/__init__.py
@@ -2,3 +2,20 @@ def setup():
     from .map.viewer import IPyLeafletMapViewer
     from glue_jupyter.registries import viewer_registry
     viewer_registry.add("map",IPyLeafletMapViewer)
+    from glue.config import settings
+    settings.SUBSET_COLORS = ["#1F78B4","#FF7F00","#E31A1C","#FB9A99"]
+
+
+    red_to_green = ['#D9838D','#ECDF0B','#7CB6BD','#77A865']
+    from glue.config import colormaps
+    from matplotlib.colors import ListedColormap
+    import matplotlib.cm as cm
+
+
+    colormaps.add('Reds', cm.Reds)
+    colormaps.add('Greens', cm.Greens)
+    colormaps.add('Blues', cm.Blues)
+    colormaps.add('Purples', cm.Purples)
+    colormaps.add('Oranges', cm.Oranges)
+
+    colormaps.add('red_to_green',ListedColormap(red_to_green, name='red_to_green'))
