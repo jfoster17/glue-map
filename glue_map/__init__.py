bf6658cc7dc3fa78243c632175c384706c8914db
Jonathan Foster <jonathan.bruce.foster@gmail.com>
2024-02-15 14:53:03 -0500

    Change subset colors

--- glue_map/__init__.py
+++ glue_map/__init__.py
@@ -6,7 +6,7 @@ def setup():
     viewer_registry.add("map", IPyLeafletMapViewer)
     from glue.config import settings
 
-    settings.SUBSET_COLORS = ["#1F78B4", "#FF7F00", "#E31A1C", "#FB9A99"]
+    settings.SUBSET_COLORS = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
 
     red_to_green = ["#D9838D", "#ECDF0B", "#7CB6BD", "#77A865"]
     green_to_red = ["#77A865", "#7CB6BD", "#ECDF0B", "#D9838D"]
